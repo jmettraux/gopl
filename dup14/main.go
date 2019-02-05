31aa8d57f42587ac8ee4b44d7131c3aca31bd301
John Mettraux <jmettraux@gmail.com>
2019-02-05 11:59:39 +0900

    Reformat

--- dup14/main.go
+++ dup14/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-    "bufio"
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func hasDuplicateLines(path string) {
-  seenLines := make(map[string]bool)
-  f, r := os.Open(path)
-  if r != nil {
-    fmt.Fprintf(os.Stderr, "%s: couldn't read file %s\n", os.Args[0], path)
-  } else {
-    s := bufio.NewScanner(f)
-    for s.Scan() {
-      if len(s.Text()) > 0 && seenLines[s.Text()] {
-        fmt.Printf("%s: >%s<\n", path, s.Text())
-        break
-      } else {
-        seenLines[s.Text()] = true
-      }
-    }
-  }
+	seenLines := make(map[string]bool)
+	f, r := os.Open(path)
+	if r != nil {
+		fmt.Fprintf(os.Stderr, "%s: couldn't read file %s\n", os.Args[0], path)
+	} else {
+		s := bufio.NewScanner(f)
+		for s.Scan() {
+			if len(s.Text()) > 0 && seenLines[s.Text()] {
+				fmt.Printf("%s: >%s<\n", path, s.Text())
+				break
+			} else {
+				seenLines[s.Text()] = true
+			}
+		}
+	}
 }
 
 func main() {
-  for _, path := range os.Args[1:] {
-    hasDuplicateLines(path)
-  }
+	for _, path := range os.Args[1:] {
+		hasDuplicateLines(path)
+	}
 }
-
